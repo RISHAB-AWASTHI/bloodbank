ad983b33b8013efdcb66273ebc144d1b6e81c8d5
Rishab <awasthirishabh56@gmail.com>
2023-05-17 20:50:54 +0530

    homepage commit

--- app/src/main/java/com/example/bloodbank/Activities/MainActivity.java
+++ app/src/main/java/com/example/bloodbank/Activities/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.bloodbank.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.bloodbank.R;
 
@@ -12,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(item.getItemId() == R.id.search_button){
+                    startActivity(new Intent(MainActivity.this, SearchActivity.class));
+                }
+                return false;
+            }
+        });
     }
 }
